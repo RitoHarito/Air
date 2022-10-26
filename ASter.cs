b66beda06eba27f59796d557ffd2a28c12913757
RitoHarito <38727197+RitoHarito@users.noreply.github.com>
2022-10-26 18:51:48 +0900

    Add files via upload

--- ASter.cs
+++ ASter.cs
@@ -10,74 +10,135 @@ public class ASter : MonoBehaviour
     [Header("WorldSetting")]
     public Vector2Int worldSize;
     public Vector2Int origin;
-    [Header("Start")]
+    [Header("PointSetting")]
     public Vector2Int startPoint;
     public Vector2Int endPoint;
     [Header("Step")]
-    [Range(1, 100)] public int step;
+    [Range(1, 1000)] public int step;
+    [Range(1, 1000)] public int step2;
     [Header("Gizmo")]
     [SerializeField] private float gizmoSize;
     private float[,] grid;//0=normal -1=wall -2=close 
     private Vector2Int nextGrid;
-    private List<Vector2Int> NextGridList = new List<Vector2Int>();
-    private List<Vector2Int> ResultGridList = new List<Vector2Int>();
+    private Vector2Int pathNextGrid;
+    private List<Vector2Int> nextGridList = new List<Vector2Int>();
+    private List<Vector2Int> resultGridList = new List<Vector2Int>();
+    private List<Vector2Int> pathGridList = new List<Vector2Int>();
     public bool GO;
-    private void OnValidate()
-    {
-        GO = true;
-    }
+    [SerializeField] private float rayHeight;
+    public bool finded;
     private void OnDrawGizmos()
     {
         if (GO)
         {
-            //GO = false;
-            grid = new float[worldSize.x, worldSize.y];
             Gizmos.DrawSphere(ConvertVector2Int(startPoint), gizmoSize);
             Gizmos.DrawSphere(ConvertVector2Int(endPoint), gizmoSize);
-            NextGridList = new List<Vector2Int>();
-            ResultGridList = new List<Vector2Int>();
-            for (int i = 0; i < worldSize.x; i++)
-            {
-                for (int ii = 0; ii < worldSize.y; ii++)
-                {
-                    grid[i, ii] = 0;
-                }
-            }
+            Initialize();
             for (int i = 0; i < step; i++)
             {
-                if (i == 0)
-                {
-                    Check8Direction(startPoint);//Handles.Label(ConvertVector2Int(left) + Vector3.up, $"<color=#ff0000>{Cost(left)}</color>", gUIStyle);
-                }
-                else
-                {
-                    Check8Direction(nextGrid);
-                }
-                nextGrid = CheckNextThisGrid(NextGridList);
-                if (!ResultGridList.Exists(x => x == nextGrid))
-                {
-                    ResultGridList.Add(nextGrid);
-                }
+                Search(i);
+                if (finded) { break; }
             }
-            //for (int i = 0; i < NextGridList.Count; i++)
-            //{
-            //    var dbg_str = NextGridList[i];
-            //    Handles.Label(ConvertVector2Int(NextGridList[i]) + Vector3.up, $"<color=#ff0000>{dbg_str}</color>", gUIStyle);
-            //}
-            for (int i = 0; i < ResultGridList.Count; i++)
+            for (int i = 0; i < step2; i++)
             {
-                Gizmos.DrawSphere(ConvertVector2Int(ResultGridList[i]), gizmoSize);
+                PathFind(i);
+            }
+            for (int i = 0; i < resultGridList.Count; i++)
+            {
+                var dbg_str = grid[resultGridList[i].x, resultGridList[i].y].ToString();
+                Handles.Label(ConvertVector2Int(resultGridList[i]) + Vector3.up, $"<color=#000000>{dbg_str}</color>", gUIStyle);
+                //Gizmos.DrawSphere(ConvertVector2Int(resultGridList[i]), gizmoSize);
+            }
+            for (int i = 0; i < pathGridList.Count; i++)
+            {
+                Gizmos.DrawSphere(ConvertVector2Int(pathGridList[i]), gizmoSize);
             }
             for (int i = 0; i < worldSize.x; i++)
             {
                 for (int ii = 0; ii < worldSize.y; ii++)
                 {
                     var thisGrid = new Vector3(origin.x + i, 0, origin.y + ii);
-                    //grid[i, ii] = 0;
                     var dbg_str = grid[i, ii];
-                    Handles.Label(thisGrid, $"<color=#000000>{dbg_str}</color>", gUIStyle);
+                    //Handles.Label(thisGrid + Vector3.up, $"<color=#000000>{dbg_str}</color>", gUIStyle);
                 }
             }
+        }
+    }
+    void PathFind(int i)
+    {
+        if (!finded) { return; }
+        nextGridList = new List<Vector2Int>();
+        if (i == 0) { Path_Check8Direction(endPoint); }
+        else { Path_Check8Direction(pathNextGrid); }
+        pathNextGrid = Path_CheckNextThisGrid(nextGridList);
+        if (pathNextGrid != startPoint)
+        {
+            pathGridList.Add(pathNextGrid);
+        }
+    }
+    private void Path_Check8Direction(Vector2Int thisGrid)
+    {
+        var up = thisGrid + Vector2Int.up;
+        var upright = thisGrid + Vector2Int.up + Vector2Int.right;
+        var right = thisGrid + Vector2Int.right;
+        var rightdown = thisGrid + Vector2Int.down + Vector2Int.right;
+        var down = thisGrid + Vector2Int.down;
+        var downleft = thisGrid + Vector2Int.down + Vector2Int.left;
+        var left = thisGrid + Vector2Int.left;
+        var leftup = thisGrid + Vector2Int.left + Vector2Int.up;
+        Path_AddNextGridList(up);
+        Path_AddNextGridList(upright);
+        Path_AddNextGridList(right);
+        Path_AddNextGridList(rightdown);
+        Path_AddNextGridList(down);
+        Path_AddNextGridList(downleft);
+        Path_AddNextGridList(left);
+        Path_AddNextGridList(leftup);
+    }
+    private void Path_AddNextGridList(Vector2Int thisGrid)
+    {
+        if (resultGridList.Exists(x => x == thisGrid)) { nextGridList.Add(thisGrid); }
+    }
+    private Vector2Int Path_CheckNextThisGrid(List<Vector2Int> grids)
+    {
+        var result = new Vector2Int();
+        var costs_tuple = new List<(float cost, Vector2Int grid)>();
+        var costs = new List<float>();
+        foreach (var thisGrid in grids)
+        {
+            var cost = grid[thisGrid.x, thisGrid.y]; costs.Add(cost);
+            var cost_tuple = (cost, thisGrid); costs_tuple.Add(cost_tuple);
+        }
+        var lowestCost = Mathf.Min(costs.ToArray());
+        result = costs_tuple.Find(x => x.cost == lowestCost).grid;
+        return result;
+    }
+    private void Initialize()
+    {
+        grid = new float[worldSize.x, worldSize.y];
+        nextGridList = new List<Vector2Int>();
+        resultGridList = new List<Vector2Int>();
+        pathGridList = new List<Vector2Int>();
+        for (int i = 0; i < worldSize.x; i++)
+        {
+            for (int ii = 0; ii < worldSize.y; ii++)
+            {
+                var thisGrid = new Vector3(origin.x + i, 0, origin.y + ii);
+                var thisWorldGrid = ConvertVector3(thisGrid);
+                if (CheckWall(thisGrid)) { SetGrid(thisWorldGrid, -1); }
+                else { SetGrid(thisWorldGrid, 0); }
+            }
+        }
+    }
+    private void Search(int i)
+    {
+        finded = nextGrid == endPoint;
+        if (i == 0) { Check8Direction(startPoint); }
+        else { Check8Direction(nextGrid); }
+        nextGrid = CheckNextThisGrid(nextGridList);
+        if (!resultGridList.Exists(x => x == nextGrid))
+        {
+            resultGridList.Add(nextGrid);
         }
     }
     private void Check8Direction(Vector2Int thisGrid)
@@ -99,6 +160,10 @@ public class ASter : MonoBehaviour
         AddNextGridList(left);
         AddNextGridList(leftup);
     }
+    private void AddNextGridList(Vector2Int thisGrid)
+    {
+        if (AllowGrid(thisGrid)) { nextGridList.Add(thisGrid); }
+    }
     private Vector2Int CheckNextThisGrid(List<Vector2Int> grids)
     {
         var result = new Vector2Int();
@@ -106,7 +171,7 @@ public class ASter : MonoBehaviour
         var costs = new List<float>();
         foreach (var thisGrid in grids)
         {
-            if (!ResultGridList.Exists(x => x == thisGrid))
+            if (!resultGridList.Exists(x => x == thisGrid))
             {
                 var cost = CalcCost(thisGrid); costs.Add(cost);
                 var cost_tuple = (cost, thisGrid); costs_tuple.Add(cost_tuple);
@@ -116,18 +181,15 @@ public class ASter : MonoBehaviour
         result = costs_tuple.Find(x => x.cost == lowestCost).grid;
         return result;
     }
-    private void AddNextGridList(Vector2Int thisGrid)
-    {
-        if (AllowGrid(thisGrid)) { NextGridList.Add(thisGrid); }
-    }
-    private float CalcCost(Vector2Int thisGrid)
-    {
-        if (AllowGrid(thisGrid)) { grid[thisGrid.x, thisGrid.y] = CostF(thisGrid); }
-        return grid[thisGrid.x, thisGrid.y];
-    }
-    private void SetCost(Vector2Int thisGrid, int set)
+    private void SetGrid(Vector2Int thisGrid, int set)
     {
         grid[thisGrid.x, thisGrid.y] = set;
+    }
+    private bool CheckWall(Vector3 thisWorldGrid)
+    {
+        var rayStart = thisWorldGrid + Vector3.up * rayHeight;
+        var rayEnd = thisWorldGrid + Vector3.down;
+        return Physics.Linecast(rayStart, rayEnd);
     }
     private bool IsWall(Vector2Int thisGrid)
     {
@@ -137,6 +199,21 @@ public class ASter : MonoBehaviour
     {
         return grid[thisGrid.x, thisGrid.y] != -1 && grid[thisGrid.x, thisGrid.y] != 0;
     }
+    private bool AllowGrid(Vector2Int thisGrid)
+    {
+        return !IsWall(thisGrid) && !IsClose(thisGrid);
+    }
+    private float CalcCost(Vector2Int thisGrid)
+    {
+        if (AllowGrid(thisGrid)) { grid[thisGrid.x, thisGrid.y] = CostF(thisGrid); }
+        return grid[thisGrid.x, thisGrid.y];
+    }
+    private float CostF(Vector2Int input)
+    {
+        float fromStartDistance = Vector2Int.Distance(input, startPoint);
+        float toEndDistance = Vector2Int.Distance(input, endPoint);
+        return fromStartDistance + toEndDistance;
+    }
     private int Cost(Vector2Int input)
     {
         var _xS = input.x - startPoint.x; var _yS = input.y - startPoint.y;
@@ -145,15 +222,6 @@ public class ASter : MonoBehaviour
         int toEndDistance = _xE * _xE + _yE * _yE;
         return fromStartDistance + toEndDistance;
     }
-    private float CostF(Vector2Int input)
-    {
-        float fromStartDistance = Vector2Int.Distance(input, startPoint);
-        float toEndDistance = Vector2Int.Distance(input, endPoint);
-        return fromStartDistance + toEndDistance;
-    }
-    private bool AllowGrid(Vector2Int thisGrid)
-    {
-        return !IsWall(thisGrid) && !IsClose(thisGrid);
-    }
-    Vector3 ConvertVector2Int(Vector2Int input) { return new Vector3(input.x, 0, input.y); }
+    private Vector3 ConvertVector2Int(Vector2Int input) { return new Vector3(input.x, 0, input.y); }
+    private Vector2Int ConvertVector3(Vector3 input) { return new Vector2Int((int)input.x, (int)input.z); }
 }
